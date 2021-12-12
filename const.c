47c0e35df23237def39a6580bcfbc82a189dcea8
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-12-12 12:54:59 +0530

    Add files via upload

---
+++ const.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14;
+    printf("%f",pi);
+    pi = 5.0;
+    printf("%f",pi);
+    const float pi2 = 3.14;
+    printf("%f",pi2);
+    pi2 = 5;
+    printf("%f",pi2);
+}
